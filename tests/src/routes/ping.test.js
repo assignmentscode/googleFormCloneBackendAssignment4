58fc1e570d2ca7a5bbbc1cc21e375dbd367c3066
Shashwat Sinha <shashwat_sinha@external.mckinsey.com>
2019-02-27 12:38:56 +0530

    Basic Health Check Server

---
+++ tests/src/routes/ping.test.js
@@ -0,0 +1,30 @@
+const { server } = require('./../../../src/server');
+
+describe('Route /ping', () => {
+  const options = {
+    method: 'GET',
+    url: '/ping',
+  };
+  it('should return status Code 200', async () => {
+    const response = await server.inject(options);
+    expect(response.statusCode).toEqual(200);
+  });
+  it('should return "pong" as a response', async () => {
+    const response = await server.inject(options);
+    expect(response.result).toEqual('pong');
+  });
+});
+describe('Route /Ping', () => {
+  const options = {
+    method: 'GET',
+    url: '/Ping',
+  };
+  it('should return status Code 404', async () => {
+    const response = await server.inject(options);
+    expect(response.statusCode).toEqual(404);
+  });
+  it('should return "pong" as a response', async () => {
+    const response = await server.inject(options);
+    expect(response.result.message).toEqual('Not Found');
+  });
+});
